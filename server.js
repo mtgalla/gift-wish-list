55dcdd9ebae8941bdf45cf41e22c3fd8b938a00a
Jesse Chen <811337@gmail.com>
2019-07-10 18:55:53 -0500

    test

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const session = require('express-session');
+const passport = require('./passport');
+
 const routes = require('./routes');
+const user = require('./routes/user');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  }),
+);
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('dev'));
@@ -15,7 +26,22 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
+// Sessions
+app.use(
+  session({
+    secret: 'fraggle-rock',
+    resave: true,
+    saveUninitialized: true,
+  }),
+);
+
+// Passport
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Routes
 app.use(routes);
+app.use('/user', user)
 
 mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ticket-tracker', { useNewUrlParser: true });
