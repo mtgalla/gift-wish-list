fc5b38d80041b82eaff543fc9ced8f752b18cca3
Turbo <toan.takahara@gmail.com>
2019-07-15 10:42:53 -0500

    Fixed chat position, add in some styler for btn

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./style.css";
-import Chat from "../../Chat"
+import Chat from "../chat"
 
 function Jumbotron({ children }) {
   return (
     <div className = "Jumbotron">
       {children}
-      <div className="chat col-6"><Chat ></Chat></div>
+      <div className="chat col-12"><Chat ></Chat></div>
       <br></br><br></br>
     </div>
   );
