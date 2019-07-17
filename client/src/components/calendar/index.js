6d7d47edc5085467bfbbc38bef016b78abe4b8ce
eggyhead <erika.eggemeyer@builtin.com>
2019-07-16 18:08:44 -0600

    removes chat

--- client/src/components/calendar/index.js
+++ client/src/components/calendar/index.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import Calendar from 'react-calendar';
+import moment from 'moment'
  
 class DateCalendar extends Component {
   state = {
     date: new Date(),
   }
  
-  onChange = date => this.setState({ date })
+  onChange = date => {
+    this.setState({ date })
+    const formatted = moment.utc(date).format()
+    
+  }
  
   render() {
     return (
