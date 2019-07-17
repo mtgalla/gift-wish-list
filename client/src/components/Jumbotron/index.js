33941b58fcefeb6d1d28c237f2cd3c5dfc65a080
eggyhead <erika.eggemeyer@builtin.com>
2019-07-16 18:12:28 -0600

    removes chat display

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./style.css";
-import Chat from "../chat"
 
 function Jumbotron({ children }) {
   return (
     <div className = "Jumbotron">
       {children}
-      <div className="chat col-12"><Chat ></Chat></div>
       <br></br><br></br>
     </div>
   );
